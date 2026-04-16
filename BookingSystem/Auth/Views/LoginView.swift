ea45e6241979b430c00ecdc3b1da1673d7ead55c
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-16 20:48:50 +0200

    Got rid of form in ui and tried the if user is signed in go to contentview

--- BookingSystem/Auth/Views/LoginView.swift
+++ BookingSystem/Auth/Views/LoginView.swift
@@ -11,36 +11,49 @@ struct LoginView: View {
     @State private var viewModel = LoginViewModel()
     
     var body: some View {
+        if viewModel.isSignedIn == true {
+            ContentView()
+                .environment(viewModel)
+        } else {
+            NavigationStack {
+                VStack(spacing: 20){
+                    
+                        TextField("Email", text: $viewModel.email)
+                            .textFieldStyle(DefaultTextFieldStyle())
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $viewModel.password)
+                            .textFieldStyle(DefaultTextFieldStyle())
+                        
+                        if !viewModel.errorMessage.isEmpty {
+                            Text(viewModel.errorMessage)
+                                .foregroundColor(.red)
+                        }
+                        
+                        Button {
+                            viewModel.login()
+                                
         
-        NavigationStack {
-            VStack{
-                List {
-                    TextField("Email", text: $viewModel.email)
-                        .textFieldStyle(DefaultTextFieldStyle())
-                        .autocapitalization(.none)
-                    SecureField("Password", text: $viewModel.password)
-                        .textFieldStyle(DefaultTextFieldStyle())
+                        } label: {
+                            Text("Login")
+                        }
+                        
+                        
                     
-                    if !viewModel.errorMessage.isEmpty {
-                        Text(viewModel.errorMessage)
-                            .foregroundColor(.red)
-                    }
-                    
-                    Button {
-                        viewModel.login()
-                    } label: {
-                        Text("Login")
-                    }
                     
                     NavigationLink("New around here? Register") {
                         OnboardingView()
+                            .navigationBarBackButtonHidden(true)
                     }
                 }
+                
+                .padding()
+                
+                .navigationTitle("Login")
+                
             }
-            
-            .navigationTitle("Login")
-            
+            .environment(viewModel)
         }
+    
     }
 }
 
