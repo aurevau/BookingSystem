5a39c7672b8a0a95f4430c16f6a85f880b85c130
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-16 19:46:16 +0200

    Added register function to repo

---
+++ BookingSystem/Auth/Repositories/AuthRepository.swift
@@ -0,0 +1,17 @@
+//
+//  AuthRepository.swift
+//  BookingSystem
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-16.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthRepository {
+    func register(email: String, password: String) async throws -> AuthDataResult {
+        try await Auth.auth().createUser(withEmail: email, password: password)
+    }
+    
+    
+}
---
+++ BookingSystem/Auth/ViewModels/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  BookingSystem
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-16.
+//
+
+import Foundation
