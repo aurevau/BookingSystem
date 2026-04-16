b9beee7208f4b15aae3bfe732c07425eb730a4a0
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-16 19:24:16 +0200

    Initial Commit

---
+++ BookingSystem/BookingSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingSystemApp.swift
+//  BookingSystem
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-16.
+//
+
+import SwiftUI
+
+@main
+struct BookingSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
