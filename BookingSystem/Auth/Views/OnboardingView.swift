3d08c2ef7bdf2d5fa8ad03473a1d9a60e8ddae73
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-16 20:48:10 +0200

    initial ui for onboarding to test register

--- BookingSystem/Auth/Views/OnboardingView.swift
+++ BookingSystem/Auth/Views/OnboardingView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var viewModel = RegisterViewModel()
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 20) {
+            
+                    TextField("Username", text: $viewModel.name)
+                        .autocorrectionDisabled()
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    TextField("Email", text: $viewModel.email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                    
+                    SecureField("Password", text: $viewModel.password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    SecureField("Confirm Password", text: $viewModel.confirmPassword)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    if !viewModel.errorMessage.isEmpty {
+                        Text(viewModel.errorMessage)
+                            .foregroundStyle(.red)
+                        
+                    }
+                    
+                    Button {
+                        viewModel.register()
+                           
+                    } label: {
+                        Text("Create Account")
+                    }
+                    
+                
+                
+                NavigationLink("Already a user?") {
+                    LoginView()
+                        .navigationBarBackButtonHidden(true)
+                }
+            }
+            .padding()
+            .navigationTitle("Welcome to registration")
+        }
+        
     }
 }
 
