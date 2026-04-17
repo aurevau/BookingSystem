1a827ffafe6b73c9fee1ffd37c08a1b134dcd7cb
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-17 14:56:26 +0200

    Added temporary logout button to mainView

--- BookingSystem/Views/MainView.swift
+++ BookingSystem/Views/MainView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct MainView: View {
+    @Environment(AuthViewModel.self) private var viewModel
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Button("Log Out") {
+            viewModel.logOut()
+        }
     }
 }
 
