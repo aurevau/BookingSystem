ecf04f9533aa75656d9caac2edd624d00f5e718d
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-17 17:30:20 +0200

    Save user function in repo

--- BookingSystem/User/Repositories/UserRepository.swift
+++ BookingSystem/User/Repositories/UserRepository.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+class UserRepository {
+    
+    private var db = Firestore.firestore()
+    
+    
+    func saveUserToFirebase(accountNumber: Int, email: String, username: String) async throws {
+        
+        let newUser = User(accountNumber: accountNumber, name: username, email: email, joined: Date().timeIntervalSince1970)
+        
+        try db.collection("users")
+            .document(newUser.id)
+            .setData(from: newUser)
+        
+        print("User saved")
+        
+    }
+}
