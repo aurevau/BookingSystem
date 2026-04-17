9170285741cf685c84f45c3af8604c9082099a2a
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-17 19:31:43 +0200

    Changed id to uid and added function to get userRole

--- BookingSystem/User/Repositories/UserRepository.swift
+++ BookingSystem/User/Repositories/UserRepository.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseFirestore
+import FirebaseAuth
 
 class UserRepository {
     
@@ -14,12 +15,27 @@ class UserRepository {
     
     func saveUserToFirebase(accountNumber: Int, email: String, name: String) async throws {
         
-        let newUser = User(accountNumber: accountNumber, name: name, email: email, joined: Date().timeIntervalSince1970)
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+
+        let newUser = User(id: uid, accountNumber: accountNumber, userRole: UserRole.user, name: name, email: email, joined: Date().timeIntervalSince1970)
         
         try db.collection("users")
             .document(newUser.id)
             .setData(from: newUser)
         
         print("User saved")
+    }
+    
+    func getUserRole() async throws -> UserRole {
+        guard let uid = Auth.auth().currentUser?.uid else {
+                return .guest
+            }
+        
+        let document = try await db.collection("users")
+            .document(uid)
+            .getDocument(as: User.self)
+        
+        return document.userRole
+        
     }
 }
