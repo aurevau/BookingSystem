7d4392995a814daad070f2e93a8c33b86f7467e2
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-17 18:19:35 +0200

    Added functionality to save user to firebase

--- BookingSystem/User/Repositories/UserRepository.swift
+++ BookingSystem/User/Repositories/UserRepository.swift
@@ -12,16 +12,14 @@ class UserRepository {
     
     private var db = Firestore.firestore()
     
-    
-    func saveUserToFirebase(accountNumber: Int, email: String, username: String) async throws {
+    func saveUserToFirebase(accountNumber: Int, email: String, name: String) async throws {
         
-        let newUser = User(accountNumber: accountNumber, name: username, email: email, joined: Date().timeIntervalSince1970)
+        let newUser = User(accountNumber: accountNumber, name: name, email: email, joined: Date().timeIntervalSince1970)
         
         try db.collection("users")
             .document(newUser.id)
             .setData(from: newUser)
         
         print("User saved")
-        
     }
 }
