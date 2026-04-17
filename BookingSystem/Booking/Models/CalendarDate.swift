ad23690913fc64798c477d21a033a85a776d42e6
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-17 21:02:30 +0200

    Handled extensions to handle calendar dates

---
+++ BookingSystem/Booking/Models/CalendarDate.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarDate.swift
+//  BookingSystem
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-17.
+//
+
+import Foundation
---
+++ BookingSystem/Booking/Views/BookingView.swift
@@ -0,0 +1,18 @@
+//
+//  BookingView.swift
+//  BookingSystem
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-17.
+//
+
+import SwiftUI
+
+struct BookingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookingView()
+}
