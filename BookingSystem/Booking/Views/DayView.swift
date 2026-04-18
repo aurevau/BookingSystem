10faa72fbaea5fdbd6cc3ea3d421d120e9b4db60
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-18 09:26:21 +0200

    Initial hardcoded ui for dayview implemented

--- BookingSystem/Booking/Views/DayView.swift
+++ BookingSystem/Booking/Views/DayView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct DayView: View {
+    @State private var dates = [
+        Date(),
+        Calendar.current.date(byAdding: .hour, value: 1, to: Date()),
+        Calendar.current.date(byAdding: .hour, value: 2, to: Date()),
+        Calendar.current.date(byAdding: .hour, value: 3, to: Date()),
+        Calendar.current.date(byAdding: .hour, value: 4, to: Date())
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text("17 April 2026")
+                 
+                Divider()
+                    .padding(.vertical)
+                Text("Välj en tid")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text("Längd: 30 minuter")
+                
+                ForEach(dates, id: \.self) {date in
+                    
+                    Button {
+                        
+                    } label: {
+                        Text((date?.timeFromDate())!)
+                            .fontWeight(.bold)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke()
+                                )
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationTitle("Fredag")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    DayView()
+    NavigationStack {
+        DayView()
+    }
+   
 }
