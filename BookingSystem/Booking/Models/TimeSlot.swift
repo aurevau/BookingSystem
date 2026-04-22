bc2f393224d67f0a042bf858bf6199fbd30f0db6
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-22 10:32:35 +0200

    added timeslot model

--- BookingSystem/Booking/Models/TimeSlot.swift
+++ BookingSystem/Booking/Models/TimeSlot.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct TimeSlot: Identifiable, Codable, Hashable {
+    let id: String
+    var start: Date
+    var end: Date
+    var createdBy: String
+    var capacity: Int
+    var isBooked: Bool
+    var bookedBy: String?
+    var bookedByName: String?
+    var bookedByEmail: String?
+    var notes: String?
+    
+}
